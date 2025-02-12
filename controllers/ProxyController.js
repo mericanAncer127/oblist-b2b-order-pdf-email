5e2f40cfa7b568de26e4a26c76d6635005b8dc3a
mericanAncer <vicenteputheany701@gmail.com>
2025-02-12 18:31:23 +0100

    log changed

--- controllers/ProxyController.js
+++ controllers/ProxyController.js
@@ -2,8 +2,6 @@ const axios = require("axios");
 exports.createInvoice = async (req, res) => {
   const { payload } = req.body;
 
-  console.log("req body: ", req.body);
-  console.log("payload: ", payload);
   try {
     const response = await axios.post(
       "https://app.pennylane.com/api/external/v1/customer_invoices",
@@ -16,11 +14,11 @@ exports.createInvoice = async (req, res) => {
       }
     );
 
-    console.log(response);
     if(response.status == 422)
     {
       return res.json({ message : "Already created"});
     }
+    console.log(response.data);
 
     res.json(response.data);
   } catch (error) {
