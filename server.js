ec943fc2de0f956b46b46db62fe33e119b2222a2
mericanAncer <vicenteputheany701@gmail.com>
2025-02-12 16:34:28 +0100

    bodyparser added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser'); 
 
 const proxyRoutes = require("./routes/invoice");
 
@@ -15,7 +15,7 @@ async function start() {
   // App
   const app = express();
   app.use(cors())
-  app.use(cookieParser())
+  app.use(bodyParser.json());  // This middleware parses incoming JSON payloads
 
   // Health check
   app.get("/health", (req, res) => {
